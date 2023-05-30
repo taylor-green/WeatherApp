051569b213f5daa107f71629854aba54f50b4a78
taylor-green <taylor.green2804@gmail.com>
2023-05-30 12:57:35 -0400

    added event listener, local storage functionality and reset form

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,3 +7,32 @@ const cityInput = document.querySelector('#city-input');
 const searchHistory = document.querySelector('#search-history');
 const currentWeather = document.querySelector('#current-weather');
 const forecast = document.querySelector('#forecast');
+
+// Initialize variables
+let city = '';
+let history = [];
+
+// Load search history from localStorage
+if (localStorage.getItem('history')) {
+    history = JSON.parse(localStorage.getItem('history'));
+    renderSearchHistory();
+}
+
+// Listen for form submit event
+searchForm.addEventListener('submit', e => {
+    e.preventDefault();
+    city = cityInput.value.trim();
+
+    if (city) {
+        // Save search history to localStorage
+        history.push(city);
+        localStorage.setItem('history', JSON.stringify(history));
+
+        // Update UI
+        renderCurrentWeather();
+        renderForecast();
+        renderSearchHistory();
+    }
+        // Reset form
+        searchForm.reset();
+    });
