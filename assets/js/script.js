956f3fb0d097d532612ad768bf62ca1152fb953b
taylor-green <taylor.green2804@gmail.com>
2023-05-30 12:35:03 -0400

    began js code and established API key and API URL

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+const API_KEY = '03314e65cfbfc21bd2e2fc95c9133396';
+const BASE_URL = 'https://api.openweathermap.org/data/2.5';
+
+// Select DOM elements
+const searchForm = document.querySelector('#search-form');
+const cityInput = document.querySelector('#city-input');
+const searchHistory = document.querySelector('#search-history');
+const currentWeather = document.querySelector('#current-weather');
+const forecast = document.querySelector('#forecast');
