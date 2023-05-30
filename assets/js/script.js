65f1419ddeb2739f7eb89e9996a2dbaf1d8aa822
taylor-green <taylor.green2804@gmail.com>
2023-05-30 14:02:29 -0400

    added 5-day forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -36,3 +36,64 @@ searchForm.addEventListener('submit', e => {
         // Reset form
         searchForm.reset();
     });
+
+    // Listen for click event on search history items
+searchHistory.addEventListener('click', e => {
+    if (e.target.classList.contains('search-history-item')) {
+        city = e.target.textContent;
+        renderCurrentWeather();
+        renderForecast();
+        setActiveHistoryItem(e.target);
+    }
+});
+
+// Render current weather for the selected city
+async function renderCurrentWeather() {
+    try {
+        const response = await fetch(`${BASE_URL}/weather?q=${city}&units=imperial&appid=${API_KEY}`);
+        const data = await response.json();
+
+        // Update UI
+        currentWeather.innerHTML = `
+            <h2>${data.name}, ${data.sys.country}</h2>
+            <h3>${new Date().toLocaleDateString()}</h3>
+            <img src="https://openweathermap.org/img/w/${data.weather[0].icon}.png" alt="${data.weather[0].description}">
+            <div>
+                <p class="temp">${Math.round(data.main.temp)}°F</p>
+                <p>Humidity: ${data.main.humidity}%</p>
+                <p>Wind Speed: ${data.wind.speed} mph</p>
+            </div>
+        `;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+
+
+// Render 5-day forecast for the selected city
+async function renderForecast() {
+    try {
+        const response = await fetch(`${BASE_URL}/forecast?q=${city}&units=imperial&appid=${API_KEY}`);
+        const data = await response.json();
+
+        // Get forecast data for the next 5 days at 12:00 PM
+        const forecastData = data.list.filter(item => item.dt_txt.includes('12:00:00'));
+
+        // Update UI
+        forecast.innerHTML = '';
+        forecastData.forEach(item => {
+            forecast.innerHTML += `
+                <div class="forecast-card">
+                    <h3>${new Date(item.dt_txt).toLocaleDateString()}</h3>
+                    <img src="https://openweathermap.org/img/w/${item.weather[0].icon}.png" alt="${item.weather[0].description}">
+                    <p class="temp">${Math.round(item.main.temp)}°F</p>
+                    <p>Wind Speed: ${item.wind.speed} mph</p>
+                    <p>Humidity: ${item.main.humidity}%</p>
+                </div>
+            `;
+        });
+    } catch (error) {
+        console.error(error);
+    }
+}
